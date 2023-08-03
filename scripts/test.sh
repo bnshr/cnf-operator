79616fc97a2036f0620e1797eb0d0eff00695b16
bnshr <bmandal@redhat.com>
2023-08-03 12:10:32 +0200

    Fix error in size

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-oc patch memcached memcached-sample -n $NAMESPACE -p '{"spec":{"replicas": 5}}' --type=merge
+oc patch memcached memcached-sample -n $NAMESPACE -p '{"spec":{"size": 5}}' --type=merge
